5e97c8d9912e044578498e522fd0489560aadd70
Ilham-Faizal-Hamka <111695627+Ilham-Faizal-Hamka@users.noreply.github.com>
2022-12-02 07:38:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,29 @@ app.use(cors({
     origin: "https://new-lookist.vercel.app/"
 }))
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+
 dotenv.config({ path: ".env" });
 app.use(express.json());
 
@@ -66,3 +89,5 @@ app.use("/api/categories", categoryRoute);
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend is running.");
 });
+
+module.exports = allowCors(handler)
